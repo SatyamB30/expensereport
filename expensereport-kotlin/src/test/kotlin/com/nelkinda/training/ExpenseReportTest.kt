182ca6274c3465318d23458114615f64287e7a7c
Satyam Baldawa <satyamb@sahaj.ai>
2023-02-10 12:07:34 +0530

    Refactor meal expense test

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -3,37 +3,28 @@ package com.nelkinda.training
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
 class ExpenseReportTest {
     @Test
-    fun `should calculate total expense and meal expense`() {
-
+    fun `should calculate total expense and meal expense for dinner and breakfast`() {
         val expected = "Expenses Fri Feb 10 11:10:59 IST 2023\n" +
                 "Dinner\t100\t \n" +
                 "Breakfast\t200\t \n" +
                 "Meal expenses: 300\n" +
                 "Total expenses: 300\n"
         val baos = ByteArrayOutputStream()
-        val ps = PrintStream(baos)
-        System.setOut(ps)
 
         //Setup Data
+        val dinnerExpense = Expense()
+        dinnerExpense.type = ExpenseType.DINNER
+        dinnerExpense.amount = 100
+
+        val lunchExpense = Expense()
+        lunchExpense.type = ExpenseType.BREAKFAST
+        lunchExpense.amount = 200
 
 
-        //CallFunctionTo Print Report Output
-
-        val expense = Expense()
-        expense.type = ExpenseType.DINNER
-        expense.amount = 100
-
-        val expense1 = Expense()
-        expense1.type = ExpenseType.BREAKFAST
-        expense1.amount = 200
-
-        val expenses = arrayListOf<Expense>()
-        expenses.add(expense)
-        expenses.add(expense1)
+        val expenses = arrayListOf(dinnerExpense, lunchExpense)
 
         ExpenseReport().printReport(expenses)
 
@@ -42,11 +33,9 @@ class ExpenseReportTest {
         val actualReportStatement = result.split("\n")
         val expectedReportStatement = expected.split("\n")
 
-
         for ((index, _) in actualReportStatement.withIndex()) {
             if(index == 0) continue
             assertEquals(actualReportStatement[index],expectedReportStatement[index])
-
         }
 
 
