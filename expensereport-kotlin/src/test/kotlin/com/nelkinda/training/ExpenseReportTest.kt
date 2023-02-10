9a50cedab9ba170b3d05ec833c16070bafa03d58
Satyam Baldawa <satyamb@sahaj.ai>
2023-02-10 12:29:08 +0530

    Add test for car rental

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -5,42 +5,68 @@ import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 
 class ExpenseReportTest {
+    private val outputFromConsole = ByteArrayOutputStream()
+
     @Test
     fun `should calculate total expense and meal expense for dinner and breakfast`() {
+        //Arrange˳
         val expected = "Expenses Fri Feb 10 11:10:59 IST 2023\n" +
                 "Dinner\t100\t \n" +
                 "Breakfast\t200\t \n" +
                 "Meal expenses: 300\n" +
                 "Total expenses: 300\n"
-        val baos = ByteArrayOutputStream()
 
-        //Setup Data
-        val dinnerExpense = Expense()
-        dinnerExpense.type = ExpenseType.DINNER
-        dinnerExpense.amount = 100
-
-        val lunchExpense = Expense()
-        lunchExpense.type = ExpenseType.BREAKFAST
-        lunchExpense.amount = 200
-
-
+        val dinnerExpense = createExpense(ExpenseType.DINNER, 100)
+        val lunchExpense = createExpense(ExpenseType.BREAKFAST, 200)
         val expenses = arrayListOf(dinnerExpense, lunchExpense)
 
+//        ACT
         ExpenseReport().printReport(expenses)
 
-
-        val result = baos.toString()
+        val result = outputFromConsole.toString()
         val actualReportStatement = result.split("\n")
         val expectedReportStatement = expected.split("\n")
 
+        performAssert(actualReportStatement, expectedReportStatement)
+
+    }
+
+    @Test
+    fun `should calculate expenses for car rental and for breakfast`() {
+
+        //Arrange
+        val carRentalExpense = createExpense(ExpenseType.CAR_RENTAL, 1000)
+        val breakfastExpense = createExpense(ExpenseType.BREAKFAST, 200)
+        val expenses = arrayListOf(carRentalExpense, breakfastExpense)
+        val expected = "Expenses Fri Feb 10 11:10:59 IST 2023\n" +
+                "Car Rental\t1000\t \n" +
+                "Breakfast\t200\t \n" +
+                "Meal expenses: 200\n" +
+                "Total expenses: 1200\n"
+//        ACT
+        ExpenseReport().printReport(expenses)
+
+        val result = outputFromConsole.toString()
+        val actualReportStatement = result.split("\n")
+        val expectedReportStatement = expected.split("\n")
+
+        performAssert(actualReportStatement, expectedReportStatement)
+    }
+
+    private fun performAssert(
+        actualReportStatement: List<String>,
+        expectedReportStatement: List<String>
+    ) {
         for ((index, _) in actualReportStatement.withIndex()) {
-            if(index == 0) continue
-            assertEquals(actualReportStatement[index],expectedReportStatement[index])
+            if (index == 0) continue
+            assertEquals(actualReportStatement[index], expectedReportStatement[index])
         }
+    }
 
-
-
-
-
+    private fun createExpense(expenseType: ExpenseType, amount: Int): Expense {
+        val breakfastExpense = Expense()
+        breakfastExpense.type = expenseType
+        breakfastExpense.amount = amount
+        return breakfastExpense
     }
 }
